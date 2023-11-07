c9bad1bd7a5511586d2ee40ac154d281abcae4aa
Pankova <pankkoo89@gmail.com>
2023-11-07 23:11:03 +0300

    feat: add footer

--- driving-school/src/App.js
+++ driving-school/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Footer></Footer>
     </div>
   );
 }
--- driving-school/src/components/footer/Footer.jsx
+++ driving-school/src/components/footer/Footer.jsx
@@ -1,7 +1,25 @@
+import vkIcon from '../../images/Vk.svg'
+import odnoclassnikiIcon from '../../images/Odnoklassniki.svg'
+import mailIcon from '../../images/Mail.svg'
+import './footer.css'
 const Footer = () => {
     return ( 
         <>
-        
+            <div className="footer-container">
+                <div className="footer-work-time">
+                    <p className="footer_work-time-title">Время работы:</p>
+                    <p className="footer_work-time-p">пн-пт 08:00–17:00,<br />
+                    перерыв 12:00–13:00</p>
+                </div>
+                <div className="footer_container-socials">
+                    <div className="footer-socials">
+                       <a href=""><img src={vkIcon} alt="vk"/></a> 
+                       <a href=""><img src={odnoclassnikiIcon} alt="odn"/></a> 
+                       <a href=""><img src={mailIcon} alt="mail"/></a> 
+                    </div>
+                    <p className="footer_socials-p">Разработка сайта: <a href="">ppank0</a></p>
+                </div>
+            </div>
         </>
      );
 }
