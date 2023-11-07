0e96864dc5b6fb88af0a0d22fff94519578c9b03
Pankova <pankkoo89@gmail.com>
2023-11-08 00:02:32 +0300

    feat: add in home page hero-section

---
+++ driving-school/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import { Button, Container } from 'react-bootstrap';
+import Header from '../components/header/Header'
+import Footer from '../components/footer/Footer'
+import heroImg from '../images/hero-img.png'
+import './home.css'
+const Home = () => {
+    return ( 
+        <>
+            <Header/>
+            <Container className='home-hero-section'>
+                <div className="hero-section-text-box">
+                    <p className="hero_section_text-box-p">Приглашаем присоединиться к нашей автошколе ДОСААФ города 
+                    Быхов и начать ваше увлекательное и безопасное дорожное путешествие.</p>
+                    <Button className='hero-button'>Записаться</Button>
+                </div>
+
+                <img src={heroImg} alt="" className="img-fluid hero-section-img"/>
+            </Container>
+            <Footer/>
+        </>
+     );
+}
+ 
+export default Home;
