d88e9b07e90c30fe6da40e323d60b17e8eb7586d
Pankova <pankkoo89@gmail.com>
2023-11-15 19:32:17 +0300

    fix: display map

--- driving-school/src/pages/Contacts.jsx
+++ driving-school/src/pages/Contacts.jsx
@@ -5,9 +5,11 @@ import arrow from './../images/arrow-contact.svg';
 import Footer from '../components/footer/Footer';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
 
 const Contacts = () => {
-    const position = [53.51557, 30.22622];
+    const position =[53.51557, 30.22622];
+    const MapIcon = new Icon({iconSize:[32,38], iconUrl:"https://cdn.icon-icons.com/icons2/1808/PNG/512/map-pin_115251.png"});
     return ( 
         <>
             <Container className='contacts-container'>
@@ -31,13 +33,14 @@ const Contacts = () => {
                 </div>
                 {/* className='contacts-map-container' */}
                 <div className="contacts-map">
-                <MapContainer center={position} zoom={9} style={{ height: '400px' }}>
+                <MapContainer center={position} zoom={17} style={{ height: '400px' }} dragging={false}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     />
-                    <Marker position={position}>
+                    <Marker position={position} icon={MapIcon}>
                         <Popup>
-                        Место, которое вы отметили.
+                            ДОСААФ г.Быхов.
                         </Popup>
                     </Marker>
                     </MapContainer>
