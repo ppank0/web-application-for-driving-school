1873ebb886751b91eb5992c546fb131c605b8451
Pankova <pankkoo89@gmail.com>
2023-12-31 05:09:20 +0300

    feat: realize component for user profile

---
+++ client/driving-school/src/components/ExamsTab.jsx
@@ -0,0 +1,37 @@
+import { useContext } from 'react';
+import { Context } from '../index';
+const ExamsTab = () => {
+    const {student} = useContext(Context);
+    const results = student.getExamResultsByStudentId(1);
+
+    const getExamName = (examId) => {
+        const exam = student.exam.find((ex) => ex.id === examId);
+        return exam ? exam.name : '';
+      };
+    
+      const getExamDate = (examId) => {
+        const exam = student.exam.find((ex) => ex.id === examId);
+        return exam ? exam.date : '';
+      };
+    
+      return (
+        <div>
+          <h2 className='mt-4'>Зачёты/экзамены</h2>
+          <div className="card">
+            <ul className="list-group list-group-flush">
+              {results.map((ex) => (
+                <li key={ex.id} className="list-group-item">
+                  <h3>{getExamName(ex.examId)}</h3>
+                  <p className="mb-1">Дата экзамена: {getExamDate(ex.examId)}</p>
+                  <p className={`mb-0 ${ex.isPassed ? 'text-success' : 'text-danger'}`}>
+                    Статус: {ex.isPassed ? 'Сдан' : 'Не сдан'}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      );
+    }
+    
+export default ExamsTab;
